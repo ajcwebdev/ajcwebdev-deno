7888d7fc72b71dcfdeea11ec8f71ce5a9790239f
Anthony Campolo <anthony@stepzen.com>
2021-07-15 01:15:08 -0700

    Update and rename index.js to index.jsx

--- index.js
+++
@@ -1,8 +0,0 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response("<h2>ajcwebdev-deno</h2>", {
-      status: 200,
-      headers: { "content-type": "text/html" },
-    }),
-  )
-})
